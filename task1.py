0257430ad9b5dd29e764a73386812210272a74ce
Ahmed <dev.ahmedhossam@gmail.com>
2024-07-01 16:00:02 +0300

    done

--- task1.py
+++ task1.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
 
+#count the number 4
 Numberslist = [5, 6, 8, 7, 4, 1, 4, 5, 5, 4, 2]
 counter1 = Numberslist.count(4)
 print("count is " + str(counter1))
  
 
+#test whether a passed letter is a vowel or not
 
+def IsVowel(letter):
+    if letter in ['a', 'i', 'e', 'o', 'u']: print("Letter is vowel")
+    else: print("Letter is consonant")
+    
+
+
+input("enter letter to check consonant or vowel:")[0]
 
---
+++ task2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+#Python program which accepts the radius of a circle from the user and compute the area
+radius = input("please enter the radius:")
+area = 3.14*( float(radius)**2)
+print("area = " + str(area))
+
