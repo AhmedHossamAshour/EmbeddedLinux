6dd349a1f6d4033f62946432d33da9a7b8b131a3
Ahmed <dev.ahmedhossam@gmail.com>
2024-07-01 13:30:48 +0300

    first commit

---
+++ task1.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+name:str = "Ahmed Hossam"
+print(name)
+print(name.upper())
+
+
+for i in range(2,6):
+    print(i)
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# this is shebang 
+
+# this is a comment
+'''
+also a comment 
+ttt
+'''
+print("done")
+
